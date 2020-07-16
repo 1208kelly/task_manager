fe5347437d669f4ab99c82a62bf8965ef0a457d8
1208kelly <1208kelly@gmail.com>
2020-07-16 18:31:15 +0000

    Adding to the database

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://user1208:************@myfirstcluster1208.14c5g.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://user1208:****@myfirstcluster1208.14c5g.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -19,14 +19,18 @@ def get_tasks():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("addtask.html")
+    return render_template("addtask.html",
+                           categories=mongo.db.categories.find())
 
 
+@app.route("/insert_task", methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
-
-if __name__ == "_main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
-
+            port=int(os.environ.get('PORT')),
+            debug=True)
