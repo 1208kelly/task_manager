505089afe663ca2ea869f278e15839ad424eecb3
1208kelly <1208kelly@gmail.com>
2020-07-16 20:08:24 +0000

    Editing mongodb info

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://user1208:0veralls4Cats1208@myfirstcluster1208.14c5g.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://user1208:*******@myfirstcluster1208.14c5g.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
