08de996137bf76e369c5d6a417ccbb5be1df1053
1208kelly <1208kelly@gmail.com>
2020-07-16 14:00:54 +0000

    Adding pymongo app

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from boson.objectid import ObjectId
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = ''
+app.config["MONGO_URI"] = 'mongodb+srv://user1208:************@myfirstcluster1208.14c5g.mongodb.net/task_manager?retryWrites=true&w=majority'
 
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World...again"
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 if __name__ == "_main__":
     app.run(host=os.environ.get('IP'),
