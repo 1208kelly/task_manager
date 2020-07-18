d8bb95b660d3f80a061c9337427bfe58cc117a93
1208kelly <1208kelly@gmail.com>
2020-07-18 17:12:16 +0000

    Adding categories display and adding categories.html file

--- app.py
+++ app.py
@@ -46,15 +46,27 @@ def edit_task(task_id):
 @app.route("/update_task/<task_id>", methods=["POST"])
 def update_task(task_id):
     tasks = mongo.db.tasks
-    tasks.update( {'_id': ObjectId(task_id)},
+    tasks.update({'_id': ObjectId(task_id)},
     {
         'task_name': request.form.get('task_name'),
-        'category_name': request.form.get('category_name'),
+        'category_name': request.form.get('category_name')
         'task_description': request.form.get('task_description'),
         'due_date': request.form.get('due_date'),
         'is_urgent': request.form.get('is_urgent')
     })
     return redirect(url_for('get_tasks'))
+
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
+
+@app.route("/get_categories")
+def get_categories():
+    return render_template('categories.html',
+    categories=mongo.db.categories.find())
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
