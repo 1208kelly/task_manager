92ef7f1681cc2157d309e5236213d27bd0249c7a
1208kelly <1208kelly@gmail.com>
2020-07-18 17:35:28 +0000

    Adding update category functionality

--- app.py
+++ app.py
@@ -56,6 +56,7 @@ def update_task(task_id):
     })
     return redirect(url_for('get_tasks'))
 
+
 @app.route("/delete_task/<task_id>")
 def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
@@ -66,6 +67,20 @@ def delete_task(task_id):
 def get_categories():
     return render_template('categories.html',
     categories=mongo.db.categories.find())
+
+
+@app.route("/edit_category/<category_id>")
+def edit_category(category_id):
+    return render_template('editcategory.html',
+    category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route("/update_category/<category_id>", methods=["POST"])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
 
 
 if __name__ == "__main__":
