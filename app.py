326e86433014f465e01679335920aa5d64290035
1208kelly <1208kelly@gmail.com>
2020-07-18 18:10:15 +0000

    Adding add category functionality

--- app.py
+++ app.py
@@ -83,6 +83,25 @@ def update_category(category_id):
     return redirect(url_for('get_categories'))
 
 
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route("/insert_category", methods=["POST"])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get('category_name')}
+    categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
+
+
+@app.route("/add_category")
+def add_category():
+    return render_template('addcategory.html')
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
